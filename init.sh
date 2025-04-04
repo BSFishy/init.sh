729c2ffe9168ebf9ac126316f7fbe5c1055740b5
Matt Provost <mattprovost6@gmail.com>
2025-04-04 14:06:51 -0500

    feat: migrate script to use setup script
    yep so now its much simpler and allows me to configure it a lot more. geegus
    
    Signed-off-by: Matt Provost <mattprovost6@gmail.com>

--- init.sh
+++ init.sh
@@ -15,22 +15,6 @@ print_step() {
 	printf "\n"
 }
 
-backup_existing_resources() {
-	local backup_dir="$HOME/home-manager-backups"
-	mkdir -p "$backup_dir"
-
-	# List of files to backup
-	local resources_to_backup=(".bashrc" ".profile" ".zshrc" ".zprofile")
-
-	for resource in "${resources_to_backup[@]}"; do
-		if [ -e "$HOME/$resource" ]; then
-			local backup_path="$backup_dir/$(basename "$resource").$(date +%Y%m%d%H%M%S)"
-			mv "$HOME/$resource" "$backup_path"
-			echo "Backed up $HOME/$resource to $backup_path"
-		fi
-	done
-}
-
 install_package() {
 	local program="$1"
 	if command -v apt-get >/dev/null 2>&1; then
@@ -49,6 +33,18 @@ ensure_installed() {
 	fi
 }
 
+sudo -v
+
+if ! command -v zsh >/dev/null 2>&1; then
+	print_step "installing zsh"
+
+	if [ -f "/etc/zsh/zshrc" ]; then
+		sudo rm /etc/zsh/zshrc
+	fi
+
+	install_package "zsh"
+fi
+
 if ! command -v nix >/dev/null 2>&1; then
 	print_step "installing nix"
 	ensure_installed "curl"
@@ -64,40 +60,13 @@ if ! command -v nix >/dev/null 2>&1; then
 	fi
 fi
 
-if ! command -v home-manager >/dev/null 2>&1; then
-	print_step "installing home-manager"
-	ensure_installed "git"
+print_step "installing configuration"
+setsid nix --extra-experimental-features "nix-command flakes" run github:BSFishy/nix-config/main </dev/tty >/dev/tty 2>&1
 
-	# backup existing stuff for initial home manager installation
-	backup_existing_resources
-
-	# add the home manager configuration
-	git clone https://github.com/BSFishy/home.nix.git ~/dotfiles
-
-	# install home manager
-	nix --extra-experimental-features "nix-command flakes" run home-manager/master -- switch --flake ~/dotfiles#server-linux
+if id "matt" >/dev/null 2>&1; then
+	current_shell=$(getent passwd matt | cut -d: -f7)
+	if [ "$current_shell" != "/bin/zsh" ]; then
+		print_step "changing shell to zsh"
+		sudo usermod -s /bin/zsh matt
+	fi
 fi
-
-if [ ! -d "$HOME/.config/nvim" ]; then
-	print_step "setting up neovim config"
-	ensure_installed "git"
-
-	mkdir -p ~/.config/nvim
-	git clone https://github.com/BSFishy/init.lua.git ~/.config/nvim
-fi
-
-if [ ! -d "$HOME/shells" ]; then
-	print_step "cloning shells project"
-	ensure_installed "git"
-
-	mkdir -p ~/shells
-	git clone https://github.com/BSFishy/shells.git ~/shells
-fi
-
-if [ ! -d "$HOME/projects" ]; then
-	mkdir -p ~/projects
-fi
-
-printf "\n\n"
-printf "success!\n"
-printf "please restart your session to enable configurations\n"
